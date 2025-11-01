4412b329a71df2722bc88bfa36c469f34d0030ef
Rob Thomson <rob@marotori.com>
2025-11-01 16:30:51 +0000

    Update PID.lua

--- scripts/inavadmin/tasks/msp/api/PID.lua
+++ scripts/inavadmin/tasks/msp/api/PID.lua
@@ -13,22 +13,73 @@ local MSP_REBUILD_ON_WRITE = false
 
 -- LuaFormatter off
 local MSP_API_STRUCTURE_READ_DATA = {
-    {field = "pid_0_P", type = "U8", apiVersion = 2.04, simResponse = {50}, min = 0, max = 1000, default = 50, help = "@i18n(api.PID_TUNING.pid_0_P)@"},
-    {field = "pid_0_I", type = "U8", apiVersion = 2.04, simResponse = {100}, min = 0, max = 1000, default = 100, help = "@i18n(api.PID_TUNING.pid_0_I)@"},
-    {field = "pid_0_D", type = "U8", apiVersion = 2.04, simResponse = {20}, min = 0, max = 1000, default = 0, help = "@i18n(api.PID_TUNING.pid_0_D)@"},
-    {field = "pid_0_FF", type = "U8", apiVersion = 2.04, simResponse = {100}, min = 0, max = 1000, default = 100, help = "@i18n(api.PID_TUNING.pid_0_F)@"},
+    -- PID 0
+    {field = "pid_0_P",  type = "U8", apiVersion = 2.04, simResponse = {15}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_0_P)@"},
+    {field = "pid_0_I",  type = "U8", apiVersion = 2.04, simResponse = {3},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_0_I)@"},
+    {field = "pid_0_D",  type = "U8", apiVersion = 2.04, simResponse = {7},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_0_D)@"},
+    {field = "pid_0_FF", type = "U8", apiVersion = 2.04, simResponse = {50}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_0_F)@"},
 
-    {field = "pid_1_P", type = "U8", apiVersion = 2.04, simResponse = {50}, min = 0, max = 1000, default = 50, help = "@i18n(api.PID_TUNING.pid_1_P)@"},
-    {field = "pid_1_I", type = "U8", apiVersion = 2.04, simResponse = {100}, min = 0, max = 1000, default = 100, help = "@i18n(api.PID_TUNING.pid_1_I)@"},
-    {field = "pid_1_D", type = "U8", apiVersion = 2.04, simResponse = {50}, min = 0, max = 1000, default = 40, help = "@i18n(api.PID_TUNING.pid_1_D)@"},
-    {field = "pid_1_FF", type = "U8", apiVersion = 2.04, simResponse = {100}, min = 0, max = 1000, default = 100, help = "@i18n(api.PID_TUNING.pid_1_F)@"},
+    -- PID 1
+    {field = "pid_1_P",  type = "U8", apiVersion = 2.04, simResponse = {15}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_1_P)@"},
+    {field = "pid_1_I",  type = "U8", apiVersion = 2.04, simResponse = {5},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_1_I)@"},
+    {field = "pid_1_D",  type = "U8", apiVersion = 2.04, simResponse = {5},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_1_D)@"},
+    {field = "pid_1_FF", type = "U8", apiVersion = 2.04, simResponse = {70}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_1_F)@"},
 
-    {field = "pid_2_P", type = "U8", apiVersion = 2.04, simResponse = {80}, min = 0, max = 1000, default = 80, help = "@i18n(api.PID_TUNING.pid_2_P)@"},
-    {field = "pid_2_I", type = "U8", apiVersion = 2.04, simResponse = {120}, min = 0, max = 1000, default = 120, help = "@i18n(api.PID_TUNING.pid_2_I)@"},
-    {field = "pid_2_D", type = "U8", apiVersion = 2.04, simResponse = {40}, min = 0, max = 1000, default = 10, help = "@i18n(api.PID_TUNING.pid_2_D)@"},
-    {field = "pid_2_FF", type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 1000, default = 0, help = "@i18n(api.PID_TUNING.pid_2_F)@"},
+    -- PID 2
+    {field = "pid_2_P",  type = "U8", apiVersion = 2.04, simResponse = {20}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_2_P)@"},
+    {field = "pid_2_I",  type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_2_I)@"},
+    {field = "pid_2_D",  type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_2_D)@"},
+    {field = "pid_2_FF", type = "U8", apiVersion = 2.04, simResponse = {100},min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_2_F)@"},
 
+    -- PID 3
+    {field = "pid_3_P",  type = "U8", apiVersion = 2.04, simResponse = {35}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_3_P)@"},
+    {field = "pid_3_I",  type = "U8", apiVersion = 2.04, simResponse = {5},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_3_I)@"},
+    {field = "pid_3_D",  type = "U8", apiVersion = 2.04, simResponse = {10}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_3_D)@"},
+    {field = "pid_3_FF", type = "U8", apiVersion = 2.04, simResponse = {30}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_3_F)@"},
+
+    -- PID 4
+    {field = "pid_4_P",  type = "U8", apiVersion = 2.04, simResponse = {70}, min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_4_P)@"},
+    {field = "pid_4_I",  type = "U8", apiVersion = 2.04, simResponse = {5},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_4_I)@"},
+    {field = "pid_4_D",  type = "U8", apiVersion = 2.04, simResponse = {8},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_4_D)@"},
+    {field = "pid_4_FF", type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0, help = "@i18n(api.PID_TUNING.pid_4_F)@"},
+
+    -- PID 5
+    {field = "pid_5_P",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_5_I",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_5_D",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_5_FF", type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+
+    -- PID 6
+    {field = "pid_6_P",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_6_I",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_6_D",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_6_FF", type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+
+    -- PID 7
+    {field = "pid_7_P",  type = "U8", apiVersion = 2.04, simResponse = {20}, min = 0, max = 255, default = 0},
+    {field = "pid_7_I",  type = "U8", apiVersion = 2.04, simResponse = {5},  min = 0, max = 255, default = 0},
+    {field = "pid_7_D",  type = "U8", apiVersion = 2.04, simResponse = {75}, min = 0, max = 255, default = 0},
+    {field = "pid_7_FF", type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0},
+
+    -- PID 8
+    {field = "pid_8_P",  type = "U8", apiVersion = 2.04, simResponse = {60}, min = 0, max = 255, default = 0},
+    {field = "pid_8_I",  type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0},
+    {field = "pid_8_D",  type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0},
+    {field = "pid_8_FF", type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0},
+
+    -- PID 9
+    {field = "pid_9_P",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_9_I",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_9_D",  type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+    {field = "pid_9_FF", type = "U8", apiVersion = 2.04, simResponse = {0}, min = 0, max = 255, default = 0},
+
+    -- PID 10
+    {field = "pid_10_P",  type = "U8", apiVersion = 2.04, simResponse = {30}, min = 0, max = 255, default = 0},
+    {field = "pid_10_I",  type = "U8", apiVersion = 2.04, simResponse = {2},  min = 0, max = 255, default = 0},
+    {field = "pid_10_D",  type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0},
+    {field = "pid_10_FF", type = "U8", apiVersion = 2.04, simResponse = {0},  min = 0, max = 255, default = 0},
 }
+
 -- LuaFormatter on
 
 local MSP_API_STRUCTURE_READ, MSP_MIN_BYTES, MSP_API_SIMULATOR_RESPONSE = core.prepareStructureData(MSP_API_STRUCTURE_READ_DATA)
